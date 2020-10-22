92f1c15b2a79b25d4843c1b612029d06dbac82e8
JonathWesley <jonathherdt@gmail.com>
2020-10-22 00:39:54 -0300

    adicionando interface

--- RBC.py
+++ RBC.py
@@ -2,8 +2,10 @@
 """
 Created on Sat Oct 17 16:21:03 2020
 
-@author: Jonath, Andriy, Matheus
+@authors: Jonath, Andriy, Matheus
 """
+
+from tkinter import *
 
 class Car:
     def __init__(self, buying, mant, doors, persons, lugBoot, safety, classi, similarity):
@@ -73,134 +75,56 @@ def convertData(data):
     arrayData.append(convertSafety(data[5]))
     
     return arrayData
-    
-if __name__ == '__main__':
-    buyMantSet = {'low', 'med', 'high', 'vhigh'}
-    doorSet = {'2', '3', '4', '5more'}
-    personSet = {'2', '4', 'more'}
-    lugBootSet = {'small', 'med', 'big'}
-    safetySet = {'low', 'med', 'high'}
-    alterWeight = {'s', 'n'}
-    
-    entryBuying = input("Insira o preço de compra (low, med, high, vhigh): ")
-    while entryBuying not in buyMantSet:
-        entryBuying = input("Insira o preço de compra (low, med, high, vhigh): ")
-        
-    entryMant = input("Insira o preço de manutenção (low, med, high, vhigh): ")
-    while entryMant not in buyMantSet:
-        entryMant = input("Insira o preço de manutenção (low, med, high, vhigh): ")
-        
-    entryDoor = input("Insira a quantidade de portas (2, 3, 4, 5more): ")
-    while entryDoor not in doorSet:
-        entryDoor = input("Insira a quantidade de portas (2, 3, 4, 5more): ")
-        
-    entryPerson = input("Insira a capacidade de pessoas (2, 4, more): ")
-    while entryPerson not in personSet:
-        entryPerson = input("Insira a capacidade de pessoas (2, 4, more): ")
-        
-    entryLugBoot = input("Insira a capacidade de bagagem (small, med, big): ")
-    while entryLugBoot not in lugBootSet:
-        entryLugBoot = input("Insira a capacidade de bagagem (small, med, big): ")
-        
-    entrySafety = input("Insira a segurança (low, med, high): ")
-    while entrySafety not in safetySet:
-        entrySafety = input("Insira a segurança (low, med, high): ")
+
+def calculateSimilarity():
+    textCars.delete(1.0,END)
     
     entry = []
-    entry.append(entryBuying)
-    entry.append(entryMant)
-    entry.append(entryDoor)
-    entry.append(entryPerson)
-    entry.append(entryLugBoot)
-    entry.append(entrySafety)
-    
-    weight = [1, 0.2, 0.5, 1, 0.75, 1]
-    
-    entryWeight = input("Deseja alterar os pesos? (s/n): ")
-    while entryWeight not in alterWeight:
-        entryWeight = input("Deseja alterar os pesos? (s/n): ")
-        
-    if entryWeight == 's':
-        while True:
-            try:
-                buyingWeight = float(input("Peso do valor de compra ("+str(weight[0])+"): "))
-            except ValueError:
-                pass
-            else:
-                break
-            
-        while True:
-            try:
-                mantWeight = float(input("Peso do valor de compra ("+str(weight[1])+"): "))
-            except ValueError:
-                pass
-            else:
-                break
-         
-        while True:
-            try:
-                doorWeight = float(input("Peso do valor de compra ("+str(weight[2])+"): "))
-            except ValueError:
-                pass
-            else:
-                break
-         
-        while True:
-            try:
-                personWeight = float(input("Peso do valor de compra ("+str(weight[3])+"): "))
-            except ValueError:
-                pass
-            else:
-                break
-            
-        while True:
-            try:
-                lugBootWeight = float(input("Peso do valor de compra ("+str(weight[4])+"): "))
-            except ValueError:
-                pass
-            else:
-                break
-            
-        while True:
-            try:
-                safetyWeight = float(input("Peso do valor de compra ("+str(weight[5])+"): "))
-            except ValueError:
-                pass
-            else:
-                break
-        
-        weight = [buyingWeight, mantWeight, doorWeight, personWeight, lugBootWeight, safetyWeight]
+    entry.append(buying.get())
+    entry.append(mant.get())
+    entry.append(doors.get())
+    entry.append(persons.get())
+    entry.append(lugBoot.get())
+    entry.append(safety.get())
     
     arrayEntry = convertData(entry)
     
-    buying = [[1, 0.66, 0.33, 0],
+    weight = []
+    weight.append(float(buyingWeight.get()))
+    weight.append(float(mantWeight.get()))
+    weight.append(float(doorsWeight.get()))
+    weight.append(float(personsWeight.get()))
+    weight.append(float(lugBootWeight.get()))
+    weight.append(float(safetyWeight.get()))
+    
+    buyingTable = [[1, 0.66, 0.33, 0],
               [0.66, 1, 0.66, 0.33],
               [0.33, 0.66, 1, 0.66],
               [0, 0.33, 0.66, 1]]
     
-    maintenance = [[1, 0.66, 0.33, 0],
+    maintenanceTable = [[1, 0.66, 0.33, 0],
                   [0.66, 1, 0.66, 0.33],
                   [0.33, 0.66, 1, 0.66],
                   [0, 0.33, 0.66, 1]]
     
-    doors = [[1, 0.66, 0.33, 0],
+    doorsTable = [[1, 0.66, 0.33, 0],
              [0.66, 1, 0.66, 0.33],
              [0.33, 0.66, 1, 0.66],
              [0, 0.33, 0.66, 1]]
     
-    persons = [[1, 0.33, 0],
+    personsTable = [[1, 0.33, 0],
                [0.33, 1, 0.66],
                [0, 0.66, 1]]
     
-    lugBoot = [[1, 0.5, 0],
+    lugBootTable = [[1, 0.5, 0],
                [0.5, 1, 0.5],
                [0, 0.5, 1]]
     
-    safety = [[1, 0.5, 0],
+    safetyTable = [[1, 0.5, 0],
               [0.5, 1, 0.5],
               [0, 0.5, 1]]
     
-    attributes = [buying, maintenance, doors, persons, lugBoot, safety]
+    attributes = [buyingTable, maintenanceTable, doorsTable, personsTable, lugBootTable, safetyTable]
     
     carList = []
     
@@ -220,14 +144,112 @@ if __name__ == '__main__':
     
     carList.sort(key=lambda x: x.similarity, reverse=True)
     
-    print('\nClasse carro de entrada: ' + carList[0].classi)
-    print('\nTop 3 carros com maior similaridade:')
-    for i in range(3):
-        print(carList[i].printCar())
-        
-    f = open("carResult.data", "w")
+    #print('\nClasse carro de entrada: ' + carList[0].classi)
+    #print('\nTop 3 carros com maior similaridade:')
+    #for i in range(3):
+    #    print(carList[i].printCar())
+    
+    labelClassResult.config(text=carList[0].classi)
+    
+    i=1
     for item in carList:
-        f.write("%s\n" % item.printCar())
-    f.close()
-
+        textCars.insert(END, str(i)+") "+item.printCar()+"\n")
+        i+=1
+        
+    #f = open("carResult.data", "w")
+    #for item in carList:
+    #    f.write("%s\n" % item.printCar())
+    #f.close()
+    
+if __name__ == '__main__':
+    root = Tk()
+    root.title("RBC")
+    
+    root.geometry("485x600")
+    
+    labelAttributes = Label(root, text="Atributos").grid(row=0, column=0)
+    labelBuying = Label(root, text="Preço de compra").grid(row=1, column=0)
+    labelMant = Label(root, text="Preço de manutenção").grid(row=2, column=0)
+    labelDoors = Label(root, text="Quantidade de portas").grid(row=3, column=0)
+    labelPersons = Label(root, text="Capacidade de pessoas").grid(row=4, column=0)
+    labelLugBoot = Label(root, text="Capacidade de bagagem").grid(row=5, column=0)
+    labelSafety = Label(root, text="Nível de segurança").grid(row=6, column=0)
+    
+    labelValues = Label(root, text="Valores").grid(row=0, column=1)
+    
+    buying = StringVar(root)
+    buying.set("low")
+    optionBuying = OptionMenu(root, buying, "low", "med", "high", "vhigh")
+    optionBuying.config(width=5)
+    optionBuying.grid(row=1, column=1)
+    
+    mant = StringVar(root)
+    mant.set("low")
+    optionMant = OptionMenu(root, mant, "low", "med", "high", "vhigh")
+    optionMant.config(width=5)
+    optionMant.grid(row=2, column=1)
+    
+    doors = StringVar(root)
+    doors.set("2")
+    optionDoors = OptionMenu(root, doors, "2", "3", "4", "5more")
+    optionDoors.config(width=5)
+    optionDoors.grid(row=3, column=1)
+    
+    persons = StringVar(root)
+    persons.set("2")
+    optionPersons = OptionMenu(root, persons, "2", "4", "more")
+    optionPersons.config(width=5)
+    optionPersons.grid(row=4, column=1)
+    
+    lugBoot = StringVar(root)
+    lugBoot.set("small")
+    optionLugBoot = OptionMenu(root, lugBoot, "small", "med", "big")
+    optionLugBoot.config(width=5)
+    optionLugBoot.grid(row=5, column=1)
+    
+    safety = StringVar(root)
+    safety.set("low")
+    optionSafety = OptionMenu(root, safety, "low", "med", "high")
+    optionSafety.config(width=5)
+    optionSafety.grid(row=6, column=1)
+    
+    labelWeights = Label(root, text="Pesos").grid(row=0, column=2)
+    
+    buyingWeight = Entry(root, width=5)
+    buyingWeight.insert(0, "1")
+    buyingWeight.grid(row=1, column=2)
+    
+    mantWeight = Entry(root, width=5)
+    mantWeight.insert(0, "0.2")
+    mantWeight.grid(row=2, column=2)
+    
+    doorsWeight = Entry(root, width=5)
+    doorsWeight.insert(0, "0.5")
+    doorsWeight.grid(row=3, column=2)
+    
+    personsWeight = Entry(root, width=5)
+    personsWeight.insert(0, "1")
+    personsWeight.grid(row=4, column=2)
+    
+    lugBootWeight = Entry(root, width=5)
+    lugBootWeight.insert(0, "0.75")
+    lugBootWeight.grid(row=5, column=2)
+    
+    safetyWeight = Entry(root, width=5)
+    safetyWeight.insert(0, "1")
+    safetyWeight.grid(row=6, column=2)
+    
+    btn = Button(root, text="Calcular", command=calculateSimilarity, bg="lightgreen").grid(row=8, column=1)
+    
+    labelClassResultText = Label(root, text="Classe carro de entrada: ").grid(row=9, column=0, sticky=W)
+    
+    labelClassResult = Label(root, bg="red")
+    labelClassResult.grid(row=9, column=0, sticky=E)
+    
+    labelText = Label(root, text="Carros com maior similaridade:").grid(row=10, column=0, sticky=W)
+    
+    textCars = Text(root, width=60)
+    textCars.grid(row=11, columnspan=3)
+    
+    root.mainloop()
     
